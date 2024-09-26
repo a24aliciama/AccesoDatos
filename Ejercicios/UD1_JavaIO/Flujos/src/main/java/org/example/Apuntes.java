eacf5207d06585e38dfcbf42b6ddde8b98a3ccad
aliciaMA <A24AliciaMA@sanclemente.local>
2024-09-26 10:58:42 +0200

    2609

--- Ejercicios/UD1_JavaIO/Flujos/src/main/java/org/example/Apuntes.java
+++ Ejercicios/UD1_JavaIO/Flujos/src/main/java/org/example/Apuntes.java
@@ -1,11 +1,41 @@
 package org.example;
 
+import java.io.Console;
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Apuntes {
+
     public static void main(String[] args) {
-        /**
-         *
-         */
+
+        Scanner sc = new Scanner(System.in);
+
+        //sc.useDelimiter("o");
+        //sc.useDelimiter("\\s*");
+        //sc.useLocale(Locale.of("es"));
+
+        //System.out.printf("%d%n %s%n %03.2f%n",Math.PI);
+        //mirar formatos de cs en overview java API
+
+
+        //System.out.println(sc.next());
+
+/*
+        Console con = System.console();
+        //System.out.println(con.readLine());
+        if (con != null){
+            System.out.println("Introduce usuario");
+            String nome = con.readLine();
+            System.out.println("Introduce contraseña");
+            char[] contra = con.readPassword();
+
+        }
+
+ */
+        //DATA INPUT OUTPUT
+
+        //rematado entrada saida .io
     }
 }
