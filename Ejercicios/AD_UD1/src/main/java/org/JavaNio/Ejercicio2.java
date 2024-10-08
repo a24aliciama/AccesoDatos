249bff01c309f2312164805d95f857819ce2d6af
aliciaMA <A24AliciaMA@sanclemente.local>
2024-10-08 10:29:32 +0200

    pp

---
+++ Ejercicios/AD_UD1/src/main/java/org/JavaNio/Ejercicio2.java
@@ -0,0 +1,32 @@
+package org.JavaNio;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Ejercicio2
+{
+    /** Escribir un programa en Java que, empleando las clases de Java NIO 2 Path y
+    File, cree un directorio (con toda la Ruta) y un archivo vacío dentro de ese directorio.
+     */
+    public static void main(String[] args) throws IOException {
+
+        Path directorio = Paths.get("C:/Users/a24aliciama/Desktop/Carpeta");
+        Path archivo = directorio.resolve("archivo.txt");
+
+        try{
+            Files.createDirectories(directorio);
+            Files.createFile(archivo);
+
+            System.out.println("directorio creado");
+            System.out.println("Archivo creado");
+
+        }catch (FileAlreadyExistsException e){
+            System.out.println("Error, ya existe el archivo");
+        }
+
+
+    }
+}
---
+++ Ejercicios/AD_UD1/src/main/java/org/JavaNio/Ejercicio3.java
@@ -0,0 +1,36 @@
+package org.JavaNio;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class Ejercicio3 {
+
+    /**
+     * Ejercicio 3. Contenido de un directorio con list.
+     * a) Escribid un programa en Java que, empleando las clases de Java NIO 2, liste los
+     * archivos de un directorio por medio del método list(). Debe mostrar sólo los
+     * archivos fuente Java, no los directorios que contiene. Recuerda el uso de filtros en
+     * Stream y de forEach.
+     * b) Haz el mismo ejercicio con el método list() de File y compara el tiempo de
+     * ejecución en cada caso
+    */
+
+    public static void main(String[] args){
+
+        Path directorio = Paths.get("C:/Users/a24aliciama/Desktop/Alky");
+
+        try (Stream<Path> archivos = Files.list(directorio)){
+            archivos.forEach(archivo ->{
+                System.out.println(archivo.getFileName());
+            });
+        }catch (IOException e){
+            System.err.println("Error " + e.getMessage());
+        }
+
+        //le falta listar solo los javas 
+    }
+
+}
