a9c8310ac90aab1aea0af7ea0512d5d354606672
aliciaMA <A24AliciaMA@sanclemente.local>
2025-02-06 10:22:16 +0100

    EjGon

---
+++ 3.Ejercicios/ClasesGon/src/main/java/GSON/EjExamen/Examen.java
@@ -0,0 +1,66 @@
+package GSON.EjExamen;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Examen {
+
+    String materia;
+    Date fecha;
+    List<String> participantes = new ArrayList<>();
+
+    public Examen(String materia, Date fecha) {
+        this.materia = materia;
+        this.fecha = fecha;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public List<String> getParticipantes() {
+        return participantes;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setParticipantes(List<String> participantes) {
+        this.participantes = participantes;
+    }
+
+    public void addParticipantes(String nombre){
+        participantes.add(nombre);
+    }
+
+    @Override
+    public String toString() {
+        return "Examen{" +
+                "materia='" + materia + '\'' +
+                ", fecha=" + fecha +
+                ", participantes=" + participantes +
+                '}';
+    }
+
+    public String toStringBuilder(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Examen{");
+                sb.append("materia='" + materia + "\'");
+                sb.append(", fecha=" + fecha );
+                sb.append(", participantes= " + participantes);
+                sb.append("}");
+
+                return sb.toString();
+    }
+}
+
